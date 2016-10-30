3114cdd7f272d01e4d3c4d0d42316b2a8324c83a
hadicahyadi <hadihadihadi91@gmail.com>
2016-10-30 21:41:58 +0700

    add get detail by table

--- src/main/java/com/curiouslabs/dao/SalesOrderDao.java
+++ src/main/java/com/curiouslabs/dao/SalesOrderDao.java
@@ -1,5 +1,6 @@
 package com.curiouslabs.dao;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
@@ -7,10 +8,14 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
 import org.apache.commons.dbutils.handlers.MapListHandler;
 import org.apache.commons.dbutils.handlers.ScalarHandler;
 
+import com.curiouslabs.dao.mapper.SalesOrderDetailMapper;
+import com.curiouslabs.dao.mapper.SalesOrderMapper;
 import com.curiouslabs.model.SalesOrder;
+import com.curiouslabs.model.SalesOrderDetail;
 import com.curiouslabs.util.Datasource;
 
 public class SalesOrderDao implements GenericDao<SalesOrder> {
@@ -52,5 +57,40 @@ public class SalesOrderDao implements GenericDao<SalesOrder> {
 		}
 		return results;
 		 
+	}
+	
+	public SalesOrder getByTable(String table){
+		String sql = "select * from sales_order where table_no = "+table;
+		SalesOrder salesOrder = new SalesOrder();
+		try {
+			salesOrder = run.query(sql, new ResultSetHandler<SalesOrder>(){
+
+				@Override
+				public SalesOrder handle(ResultSet rs) throws SQLException {
+					int index = 0;
+					SalesOrder salesOrder = new SalesOrderMapper().mapRow(rs, index);
+					return salesOrder;
+				}
+			});
+			
+			String detailSql = "select s.*,m.* from sales_order_detail s"
+					+ " join menu m on s.menu_id = m.id"
+					+ " where sales_order_id = "+salesOrder.getId();
+			
+			@SuppressWarnings("unchecked")
+			List<SalesOrderDetail> listDetail = (List<SalesOrderDetail>) run.query(detailSql, new ResultSetHandler<SalesOrderDetail>(){
+
+				@Override
+				public SalesOrderDetail handle(ResultSet rs) throws SQLException {
+					int index = 0;
+					SalesOrderDetail salesOrderDetail = new SalesOrderDetailMapper().mapRow(rs, index);
+					return salesOrderDetail;
+				}
+			});
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return salesOrder;
 	}
 }
